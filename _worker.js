d8d29e9d9b3a4d023acd9ed922796f335d8cd494
fewewf <32592150+fewewf@users.noreply.github.com>
2025-03-20 20:25:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,12 @@ let 我的UUID = "550e8400-e29b-41d4-a086-446655445800";
 let 默认节点名称 = "节点";
 
 let 优选TXT = [
-  "https://raw.githubusercontent.com/dsddr02/ipupdate/refs/heads/main/valid_ips.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/HKG.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/KHH.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/SIN.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/NRT.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/SEA.txt",
+  "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SpeedTest/LHR.txt",
   
 ];
 let 优选列表 = [];
